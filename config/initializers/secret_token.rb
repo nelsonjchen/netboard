da38c3bbf6d27fccd421ca02a256cf5ec720feed
Nelson Chen <crazysim@gmail.com>
2011-12-19 13:37:43 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netboard::Application.config.secret_token = 'a0153f8137eca995047b02fc40d660081f23b59e01837915410680d7dfe4c1ad7f326a74960531e614a44f01a669eaf698652797b15332c00bed34cb5b146b5e'
