367bfba81a669ebb6ce39060eb4191798c30b29f
Nelson Chen <crazysim@gmail.com>
2011-12-22 17:04:11 -0800

    more columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111220202145) do
+ActiveRecord::Schema.define(:version => 20111223005838) do
 
   create_table "hosts", :force => true do |t|
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "inbound",             :precision => 16, :scale => 0
+    t.decimal  "outbound",            :precision => 16, :scale => 0
+    t.boolean  "inbound_restricted"
+    t.boolean  "outbound_restricted"
   end
 
 end
