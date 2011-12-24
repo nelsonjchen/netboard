20c0ccc01a5ec13b6b2b639a0822ea5dddc2626a
Nelson Chen <crazysim@gmail.com>
2011-12-23 20:44:27 -0800

    preliminary host model finding address

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,13 @@
+require 'resolv'
+
 class Host < ActiveRecord::Base
   validates :ip_address, presence:true, uniqueness:true
+
+  def find_by_address(address)
+    self.find_by_ip_address(Resolv.getaddress(address))
+  end
+
+  def find_by_address!(address)
+    self.find_by_ip_address!(Resolv.getaddress(address))
+  end
 end
