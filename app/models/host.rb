ac05e91d43b126004be9096343626b5e3972ce47
Nelson Chen <crazysim@gmail.com>
2011-12-24 21:10:23 -0800

    moved stuff out

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,4 +1,5 @@
 require 'resolv'
+require 'net/http'
 
 class Host < ActiveRecord::Base
   validates :ip_address, presence:true, uniqueness:true
@@ -11,5 +12,24 @@ class Host < ActiveRecord::Base
 
   def self.find_by_address!(address)
     self.find_by_ip_address!(Resolv.getaddress(address))
+  end
+
+  def self.update_from_json(config_filepath = nil)
+    config_filepath = config_filepath || Rails.root.join("config/resnet.yml")
+    url = YAML.load_file(config_filepath)['stitch_url']
+    resp = Net::HTTP.get(URI(url))
+    hosts = ActiveSupport::JSON.decode(resp)
+    self.transaction do
+      hosts.each do |host|
+        mHost = self.find_or_create_by_ip_address(host[0])
+        attributes = host[1]
+
+        mHost.inbound = attributes["inbound"]
+        mHost.outbound = attributes["outbound"]
+        mHost.inbound_restricted = attributes["in_restricted"]
+        mHost.outbound_restricted = attributes["out_restricted"]
+        mHost.save!
+      end
+    end
   end
 end
--- lib/tasks/resnet.rake
+++ lib/tasks/resnet.rake
@@ -3,21 +3,6 @@ require 'net/http'
 namespace "resnet" do
   desc "Pull from Stitch"
   task :stitch_pull => :environment do
-    CONFIG = YAML.load_file(Rails.root.join("config/resnet.yml"))
-    url = CONFIG['stitch_url']
-    res = Net::HTTP.get(URI(url))
-    hosts = ActiveSupport::JSON.decode(res)
-    Host.transaction do
-      hosts.each do |host|
-        mHost = Host.find_or_create_by_ip_address(host[0])
-        attributes = host[1]
-
-        mHost.inbound = attributes["inbound"]
-        mHost.outbound = attributes["outbound"]
-        mHost.inbound_restricted = attributes["in_restricted"]
-        mHost.outbound_restricted = attributes["out_restricted"]
-        mHost.save!
-      end
-    end
+    Host.update_from_json
   end
 end
