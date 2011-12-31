7a02f6865d866a9af4033f07317546c834bf8925
Nelson Chen <crazysim@gmail.com>
2011-12-30 23:33:50 -0800

    update name

--- test/functional/status_controller_test.rb
+++ test/functional/status_controller_test.rb
@@ -16,11 +16,11 @@ class StatusControllerTest < ActionController::TestCase
     assert_equal "51.41.31.21", assigns[:address]
   end
 
-  test "should show prompt with flash for unknown ip query" do
+  test "should show prompt layout with flash for unknown ip query" do
     flunk
   end
 
-  test "should show status and prompt when visiting from inside resnet" do
+  test "should show status and prompt layout when visiting from inside resnet" do
     mock_ip("169.231.39.189")
     get :show
     assert_response :success
